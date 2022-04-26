477554c7fc91e919e7cee642d1fa11df01ae6ebb
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-04-26 17:35:40 +0100

    nuevo componente de sorteo

--- src/app/show-giveaway/show-giveaway.component.ts
+++ src/app/show-giveaway/show-giveaway.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {CartService} from "../cart.service";
-
+import { Component } from '@angular/core';
+import { LoadScriptsService} from '../load-scripts.service';
 @Component({
   selector: 'app-show-giveaway',
   templateUrl: './show-giveaway.component.html',
   styleUrls: ['./show-giveaway.component.css']
 })
 export class ShowGiveawayComponent{
+    constructor (private _LoadScripts: LoadScriptsService) {
+        _LoadScripts.Load(["giveaway_script"]);
+    }
+
 
 
 }
