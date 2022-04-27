bf13c8592298a59ccb46b89e0e2b0b10008a3adc
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-04-27 21:05:50 +0100

    Añadida funcion de agregar scripts normales al head

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { LoadScriptsService } from "./load-scripts.service";
 export class AppComponent {
 
     constructor(private _LoadScripts:LoadScriptsService) {
-        _LoadScripts.LoadUrl(["https://cdn.jsdelivr.net/npm/glider-js@1.7.7/glider.min.js", "https://code.jquery.com/jquery-3.6.0.js"]);
+        _LoadScripts.LoadUrl(["https://code.jquery.com/jquery-3.6.0.js"]);
         _LoadScripts.LoadLinks(["https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css", "https://cdn.jsdelivr.net/npm/glider-js@1.7.7/glider.min.css", "https://fonts.googleapis.com/icon?family=Material+Icons"])
     }
 
