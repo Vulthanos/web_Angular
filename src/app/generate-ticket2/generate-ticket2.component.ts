d8d4766411bfca869b881dce59d879cb1372fd10
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-05-03 20:32:42 +0100

    mejora del formulario con validacion

--- src/app/generate-ticket2/generate-ticket2.component.ts
+++ src/app/generate-ticket2/generate-ticket2.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-generate-ticket2',
   templateUrl: './generate-ticket2.component.html',
   styleUrls: ['./generate-ticket2.component.css']
 })
-export class GenerateTicket2Component implements OnInit {
+export class GenerateTicket2Component {
+    signupForm: FormGroup;
 
-  constructor() { }
+    constructor(private builder: FormBuilder) {
+        this.signupForm = this.builder.group({
+            email: ['', Validators.compose([Validators.required, Validators.email])] ,
+        });}
 
-  ngOnInit(): void {
-  }
+    submit(value:any) {
+        console.log(this.signupForm.value);
+    }
 
 }
