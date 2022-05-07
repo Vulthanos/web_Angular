6560e3db78c530c16f92e4d0560c10e0916013a5
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-05-08 00:47:03 +0100

    Logout ya recarga la página principal

--- src/assets/JS_Files/logout.js
+++ src/assets/JS_Files/logout.js
@@ -1,5 +1,13 @@
-import { setLogged } from "./firebase_logs.mjs";
+import {getLogged, setLogged} from "./firebase_logs.mjs";
+
+async function logOut() {
+    const isOut = await getLogged();
+    if (!isOut) {
+        location.href = "http://localhost:4200";
+    }
+}
 
 document.getElementById("logout").addEventListener("click", async function() {
     await setLogged(false, "not logged");
+    await logOut();
 });
