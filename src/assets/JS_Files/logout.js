cba608c5101f2ca806a95e6304ca30edb8da595e
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-05-08 01:52:43 +0100

    El perfil carga de forma dinamica el nombre y email del usuario desde firestore

--- src/assets/JS_Files/logout.js
+++ src/assets/JS_Files/logout.js
@@ -1,4 +1,4 @@
-import {getLogged, setLogged} from "./firebase_logs.mjs";
+import {getLogged, setLogged, getLoggedUser, getUserById} from "./firebase_logs.mjs";
 
 async function logOut() {
     const isOut = await getLogged();
@@ -7,7 +7,15 @@ async function logOut() {
     }
 }
 
-document.getElementById("logout").addEventListener("click", async function() {
+const userId = await getLoggedUser();
+const user = await getUserById(userId);
+const user_name = document.getElementById('user_name');
+user_name.append(user.name + " " + user.surname);
+
+const user_mail = document.getElementById('user_mail');
+user_mail.append(user.email);
+
+const logoutBtn = document.getElementById("logout");
+logoutBtn.addEventListener("click", async function() {
     await setLogged(false, "not logged");
-    await logOut();
 });
