ce83d704eb7683824abb73d0bc42c71344cf48c8
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-05-08 17:55:13 +0100

    Carga dinamica en slider-result y creacion de metodo para añadir a carrito

--- src/app/services/check-user.service.ts
+++ src/app/services/check-user.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from "@angular/fire/compat/firestore";
-import {doc, getDoc, getFirestore} from "@angular/fire/firestore";
-//import {db} from "src/environments/environment";
-
+import {doc, getDoc, getFirestore, getDocs, collection} from "@angular/fire/firestore";
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {Observable, Subject} from "rxjs";
 import {app} from "../../environments/environment";
+import { Producto } from "../interfaces/producto.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -35,6 +33,27 @@ export class CheckUserService {
         }
     }
 
+    async addProductoToUserCart(pDesc, pName, pPrice, pImg){
+        const db = getFirestore(app);
+        const docSnap = await getDoc(doc(db, "logs", "logged"));
+        if (docSnap.exists()) {
+            if(docSnap.data()['logged']){
+                const user = await getDoc(doc(db, "users", docSnap.data()['loggedUser']))
+                const userCart = user.data()['cart'];
+                const productsSnap = await getDocs(collection(db, "products"));
+                productsSnap.forEach(p => {
+                    const pData = p.data();
+                    if (pData['description'] === pDesc && pData['name'] === pName && pData['price'] === pPrice && pData['img'] === pImg) {
+                        userCart.push(p.id);
+                    }
+                });
+            } else{
+                console.log("No hay usuarios logueados");
+            }
+        } else {
+            console.log("No existe el document");
+        }
+    }
 
 }
 
